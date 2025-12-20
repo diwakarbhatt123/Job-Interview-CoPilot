5fcfcf99fdaf81071fed75568da63d0acffc120c
diwakarbhatt <diwakarbhatt68@gmail.com>
2025-12-20 23:12:34 +0000

    INT-13: M1.3 Implement user login functionality with JWT authentication

--- services/account-service/src/main/java/com/jobcopilot/account_service/AccountServiceApplication.java
+++ services/account-service/src/main/java/com/jobcopilot/account_service/AccountServiceApplication.java
@@ -3,7 +3,7 @@ package com.jobcopilot.account_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AccountServiceApplication {
 
   public static void main(String[] args) {
