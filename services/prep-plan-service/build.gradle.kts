182370bed82555805d33cddbfb1a736359c2e946
Diwakar Bhatt <diwakarbhatt68@gmail.com>
2026-01-19 01:19:00 +0000

    Update Spotless and add SpotBugs plugin

--- services/prep-plan-service/build.gradle.kts
+++ services/prep-plan-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	id("org.springframework.boot") version "4.0.1"
 	id("io.spring.dependency-management") version "1.1.7"
   id("com.diffplug.spotless") version "8.1.0"
-  id("com.diffplug.spotless") version "6.25.0"
+  id("com.github.spotbugs") version "6.4.8"
 }
 
 group = "com.jobcopilot"
